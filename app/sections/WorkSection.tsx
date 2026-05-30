c471675fc79229cf5cf4dc058f45f4effef86aa0
peterjames3 <kamauchief97@gmail.com>
2026-05-30 18:23:25 +0300

    added another case study

--- app/sections/WorkSection.tsx
+++ app/sections/WorkSection.tsx
@@ -117,7 +117,7 @@ export function WorkSection({ caseStudies }: WorkSectionProps) {
           </div>
 
           {/* Projects Grid */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {filteredProjects.map((project, index) => (
               <div
                 key={`${project.title}-${activeCategory}`}
